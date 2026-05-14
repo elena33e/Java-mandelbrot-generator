ae5edc9418a433a94a6cd4ef8482fa9cffe98504
Eremia Elena <108981608+elena33e@users.noreply.github.com>
2026-05-14 20:39:45 +0300

    Refactor Worker class to use RowManager for row distribution

--- src/main/java/com/mycompany/fractalmandelbrotjava/Worker.java
+++ src/main/java/com/mycompany/fractalmandelbrotjava/Worker.java
@@ -3,21 +3,19 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.fractalmandelbrotjava;
+
 import java.util.concurrent.atomic.AtomicLong;
 
-
 public class Worker implements Runnable {
-    private final int id;
-    private final int totalThreads;
+    private final RowManager rowManager; // Resursa partajată
     private final int width;
     private final int height;
     private final FractalConfig conf;
     private final int[] allData;
     private final AtomicLong totalIters;
 
-    public Worker(int id, int totalThreads, int width, int height, FractalConfig conf, int[] allData, AtomicLong totalIters) {
-        this.id = id;
-        this.totalThreads = totalThreads;
+    public Worker(RowManager rowManager, int width, int height, FractalConfig conf, int[] allData, AtomicLong totalIters) {
+        this.rowManager = rowManager;
         this.width = width;
         this.height = height;
         this.conf = conf;
@@ -28,9 +26,10 @@ public class Worker implements Runnable {
     @Override
     public void run() {
         long localIters = 0;
+        int y;
         
-        // Distribuție intercalată 
-        for (int y = id; y < height; y += totalThreads) {
+   
+        while ((y = rowManager.getNextRow()) != -1) {
             double py = conf.yStart() + (double) y * conf.height() / height;
             
             for (int x = 0; x < width; x++) {
@@ -38,17 +37,18 @@ public class Worker implements Runnable {
                 
                 int iter;
                 if (conf.isJulia()) {
-                    // Pentru Julia: z0 = (px, py), c = constant
                     iter = compute(px, py, conf.cRe(), conf.cIm());
                 } else {
-                    // Pentru Mandelbrot: z0 = 0, c = (px, py)
                     iter = compute(0.0, 0.0, px, py);
                 }
                 
+                // Scriere direct în vectorul partajat
                 allData[y * width + x] = iter;
                 localIters += iter;
             }
         }
+        
+        // Adunăm rezultatul local la cel global 
         totalIters.addAndGet(localIters);
     }
 
